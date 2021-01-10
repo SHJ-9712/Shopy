5290e0e1df3756d589cb66810704e56d70717d28
신혁준 <67365409+SHJ-9712@users.noreply.github.com>
2021-01-10 18:18:50 +0900

    main visual
    메인 슬라이더 완성(slick)

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,16 @@
+// big_header shopping_nav search form
 function searchForm() {
     document.getElementById("search").style.display = "block";
     document.querySelector(".search_icon").type = "submit";
 }
+// 메인 슬라이더
+$('.main_visual').slick({
+    infinite : true,
+    pauseOnHover : false,
+    pauseOnFocus: false,
+    vertical : true,
+    dots: true,
+    arrows : false,
+    autoplay: false,
+    autoplaySpeed: 2500,
+});
